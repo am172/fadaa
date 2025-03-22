02d2d0f27107ed1f0fd268ea9d1bc709a999ac73
amr roshdy shanab <120336819+am172@users.noreply.github.com>
2025-03-22 07:42:26 +0200

    Update student.jsx

--- students/src/student.jsx
+++ students/src/student.jsx
@@ -1,46 +1,15 @@
-import React, { useState } from "react";
-
-import "./assets/student.css"; 
+import "./assets/student.css";
 import { PostsDisplay } from "./Post";
-import { FaBars, FaTimes } from "react-icons/fa"; // أيقونات القائمة والإغلاق
-import { FaDownload } from "react-icons/fa";
 
-import { Link } from "react-router-dom";
-
+import NavBAR from "./compontets/navBar";
 
 const student = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false); // حالة القائمة
-
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen); // فتح/إغلاق القائمة
-    };
+    
+   
 
     return (
         <div className="home-container">
-                <nav className="top-navbar">
-            <div className="navbar-logo">جمعية فضاء</div>
-
-            {/* أيقونة القائمة */}
-            <div className="menu-icon" onClick={toggleMenu}>
-                {isMenuOpen ? <FaTimes /> : <FaBars />}
-            </div>
-
-            {/* قوائم النافبار */}
-            <ul className={`navbar-links ${isMenuOpen ? "active" : ""}`}>
-                <li>
-                    <Link to="/student/news">أخر الأخبار</Link>
-                </li>
-                <li>
-                    <Link to="/student/houses">البيوت</Link>
-                </li>
-                <li>
-                    <Link to="/student/students">الطلاب</Link>
-                </li>
-                 <li>
-                        <a href="/base1.apk" download> <FaDownload/></a>
-                    </li>
-            </ul>
-        </nav>
+            <NavBAR/>
 
             <main className="content">
                 <PostsDisplay />
